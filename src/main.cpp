13a4e3f5cd7a54e52b39f34f82ddc86e06b70cc2
Seunghyeon Nam <kuguakrguakr@gmail.com>
2021-06-08 16:45:34 +0900

    print Op and operand types

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
 #include "mlir/Parser.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"
 #include "mlir/Dialect/Linalg/IR/LinalgOps.h"
 
 #include <string>
+
+bool compare_opt(mlir::OwningModuleRef &ir_before, mlir::OwningModuleRef &ir_after) {
+    llvm::outs() << "--- IR before opt ---\n";
+    for (auto &func: *ir_before) {
+        func.walk([&](mlir::Operation *op) {
+            auto nwhc_filter_hwcf_op = mlir::dyn_cast<mlir::linalg::ConvInputNHWCFilterHWCFOp>(op);
+            if (nwhc_filter_hwcf_op) {
+                llvm::outs() << nwhc_filter_hwcf_op.getOperationName() << "\n";
+                for (auto operand : nwhc_filter_hwcf_op->getOperands()) {
+                    llvm::outs() << operand;
+                }
+                llvm::outs() << "\n";
+            }
+        });
+    }
+
+    llvm::outs() << "--- IR after opt ---\n";
+    for (auto &func: *ir_after) {
+        func.walk([&](mlir::Operation *op) {
+            if (mlir::dyn_cast<mlir::linalg::InitTensorOp>(op) || mlir::dyn_cast<mlir::linalg::GenericOp>(op) || mlir::dyn_cast<mlir::linalg::TensorReshapeOp>(op) || mlir::dyn_cast<mlir::linalg::MatmulOp>(op) || mlir::dyn_cast<mlir::linalg::YieldOp>(op)) {
+                llvm::outs() << op->getName() << "\n";
+                for (const auto &operand : op->getOperands()) {
+                    llvm::outs() << operand.getType();
+                    // if (mlir::isa<mlir::TensorType>(&operand)) {
+
+                    // }
+                    // if (mlir::isa<mlir::TensorType>(operand)) {
+                    //     llvm::outs() << operand.getType();
+                    // } else {
+                    //     llvm::outs() << "integral type ignored";
+                    // }
+                    
+                }
+                llvm::outs() << "\n";
+            }
+        });
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -29,10 +69,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    llvm::outs() << "MLIR-before\n\n";
-    ir_before->print(llvm::outs());
-    llvm::outs() << "\nMLIR-after\n\n";
-    ir_after->print(llvm::outs());
+    // llvm::outs() << "MLIR-before\n\n";
+    // ir_before->print(llvm::outs());
+    // llvm::outs() << "\nMLIR-after\n\n";
+    // ir_after->print(llvm::outs());
+
+    compare_opt(ir_before, ir_after);
+
+    // if (compare_opt(ir_before, ir_after)) {
+    //     llvm::outs() << "valid optimization\n";
+    // } else {
+    //     llvm::outs() << "INVALID optimization\n";
+    // }
 
     return 0;
 }
