bb80c29c1c7e99708d3d8b9e25d3852a4e6b2e1f
makesource <makesource@gmail.com>
2021-07-06 01:27:16 +0900

    Remove using namespace in header file

--- src/vcgen.h
+++ src/vcgen.h
@@ -4,7 +4,5 @@
 #include "mlir/IR/BuiltinOps.h"
 #include <string>
 
-using namespace std;
-
 Results verify(mlir::OwningModuleRef &src, mlir::OwningModuleRef &tgt,
             const string &dump_smt_to);
