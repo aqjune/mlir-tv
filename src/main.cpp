825e487e723d52379a88b3e14a4a4ab9d642f424
makesource <makesource@gmail.com>
2021-07-05 23:35:19 +0900

    Merge utility function into main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "smt.h"
 #include "vcgen.h"
-#include "utilities.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/PrettyStackTrace.h"
@@ -16,6 +15,9 @@
 
 using namespace std;
 using namespace mlir;
+
+using ChunkBufferHandler = llvm::function_ref<int(
+     std::unique_ptr<llvm::MemoryBuffer> srcBuffer, std::unique_ptr<llvm::MemoryBuffer> tgtBuffer)>;
 
 llvm::cl::opt<string> filename_src(llvm::cl::Positional,
   llvm::cl::desc("first-mlir-file"),
@@ -35,6 +37,10 @@ llvm::cl::opt<string> arg_dump_smt_to("dump-smt-to",
 llvm::cl::opt<bool> split_input_file("split-input-file",
   llvm::cl::desc("Split the input file into pieces and process each chunk independently"),
   llvm::cl::init(false));
+
+int splitAndProcessBuffer(std::unique_ptr<llvm::MemoryBuffer> srcBuffer,
+                                             std::unique_ptr<llvm::MemoryBuffer> tgtBuffer,
+                                             ChunkBufferHandler processChunkBuffer);
 
 int main(int argc, char* argv[]) {
   llvm::sys::PrintStackTraceOnErrorSignal(argv[0]);
@@ -93,4 +99,45 @@ int main(int argc, char* argv[]) {
   } else {
     return processBuffer(std::move(src_file), std::move(tgt_file));
   }
+}
+
+
+int splitAndProcessBuffer(std::unique_ptr<llvm::MemoryBuffer> srcBuffer,
+                                             std::unique_ptr<llvm::MemoryBuffer> tgtBuffer,
+                                             ChunkBufferHandler processChunkBuffer) {
+  const char splitMarker[] = "// -----";
+
+  SmallVector<llvm::StringRef, 8> sourceBuffers, targetBuffers;
+  auto *srcMemBuffer = srcBuffer.get();
+  auto *tgtMemBuffer = tgtBuffer.get();
+  srcMemBuffer->getBuffer().split(sourceBuffers, splitMarker);
+  tgtMemBuffer->getBuffer().split(targetBuffers, splitMarker);
+
+  // Add the original buffer to the source manager.
+  llvm::SourceMgr srcSourceMgr, tgtSourceMgr;
+  srcSourceMgr.AddNewSourceBuffer(std::move(srcBuffer), llvm::SMLoc());
+  tgtSourceMgr.AddNewSourceBuffer(std::move(tgtBuffer), llvm::SMLoc());
+
+  if (sourceBuffers.size() != targetBuffers.size()) {
+      return 1;
+  }
+
+  int hadFailure = 0;
+  for (int i = 0; i < sourceBuffers.size(); i ++) {
+    auto &sourceSubBuffer = sourceBuffers[i];
+    auto souceSplitLoc = llvm::SMLoc::getFromPointer(sourceSubBuffer.data());
+    unsigned sourceSplitLine = srcSourceMgr.getLineAndColumn(souceSplitLoc).first;
+    auto sourceSubMemBuffer = llvm::MemoryBuffer::getMemBufferCopy(sourceSubBuffer);
+
+    auto &targetSubBuffer = targetBuffers[i];
+    auto targetSplitLoc = llvm::SMLoc::getFromPointer(targetSubBuffer.data());
+    unsigned targetSplitLine = tgtSourceMgr.getLineAndColumn(targetSplitLoc).first;
+    auto targetSubMemBuffer = llvm::MemoryBuffer::getMemBufferCopy(targetSubBuffer);
+
+    if (processChunkBuffer(std::move(sourceSubMemBuffer), std::move(targetSubMemBuffer)))
+      hadFailure = 1;
+  }
+
+  // If any fails, then return a failure of the tool.
+  return hadFailure;
 }
