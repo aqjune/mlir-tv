e64e26b5d0e88d5abeeeb55ef4271706f03e387a
Seunghyeon Nam <kuguakrguakr@gmail.com>
2021-07-06 11:19:19 +0900

    Refactor RegFile to use hashing map (#19)

--- src/state.cpp
+++ src/state.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 
+ValueTy RegFile::findOrCrash(mlir::Value v) const {
+  auto itr = m.find(v);
+  if (itr != m.end()) {
+    return itr->second;
+  } else {
+    llvm::errs() << "Cannot find key: " << v << "\n";
+    llvm_unreachable("Unknown key");
+  }
+}
+
 void RegFile::add(mlir::Value v, ValueTy &&t) {
   assert(!contains(v));
-  m.emplace_back(v, std::move(t));
+  m.insert({v, std::move(t)});
 }
 
 bool RegFile::contains(mlir::Value v) const {
-  for (auto &[a, b]: m) {
-    if (a == v)
-      return true;
-  }
-  return false;
+  return (bool)m.count(v);
 }
 
 z3::expr RegFile::getZ3Expr(mlir::Value v) const {
-  for (auto &[a, b]: m) {
-    if (a != v) continue;
-    z3::expr e(ctx);
-    visit([&](auto &&itm) {
-      e = (z3::expr)itm;
-    }, b);
-    return e;
-  }
-
-  llvm::errs() << "Cannot find key: " << v << "\n";
-  assert(false && "Unknown key");
+  auto var = findOrCrash(v);
+  z3::expr e(ctx);
+  visit([&](auto &&itm) {
+    e = (z3::expr)itm;
+  }, var);
+  return e;
 }
-
 
 llvm::raw_ostream& operator<<(llvm::raw_ostream& os, State &s) {
   for (auto itm: s.regs) {
