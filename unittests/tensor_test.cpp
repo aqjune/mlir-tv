91cfe1c7ef3301207dfe8d47d646cf87d0a35c1b
Seunghyeon Nam <kuguakrguakr@gmail.com>
2021-07-11 13:20:01 +0900

    Prototype unit tests (#25)

--- unittests/tensor_test.cpp
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-#include "src/value.h"
-
-TEST(UnitTensorTest, SampleTest)
-{
-    EXPECT_EQ(1, 1);
-}
---
+++ unittests/tv_test_shared.h
@@ -0,0 +1,2 @@
+#define EXPECT_Z3_EQ(lhs, rhs) EXPECT_EQ((Z3_ast)lhs, (Z3_ast)rhs)
+#define EXPECT_Z3_NE(lhs, rhs) EXPECT_NE((Z3_ast)lhs, (Z3_ast)rhs)
