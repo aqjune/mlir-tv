ef5ff6c9963b7a983b6afc1bd5685469a17bc221
makesource <makesource@gmail.com>
2021-07-19 15:24:45 +0900

    Implement array-based memblocks encoding

--- src/memory.cpp
+++ src/memory.cpp
@@ -38,3 +38,55 @@ std::pair<z3::expr, z3::expr> Memory::load(const z3::expr &bid, const z3::expr &
   const auto block = getMemBlock(bid);
   return {z3::select(block.array, idx), z3::ult(idx, block.numelem)};
 }
+
+////
+
+NewMemory::NewMemory(unsigned int NUM_BLOCKS):
+  NUM_BLOCKS(NUM_BLOCKS),
+  arrayMaps(NUM_BLOCKS, ctx.constant("arrayMap",
+    ctx.array_sort(ctx.bv_sort(BID_BITS), ctx.array_sort(Index::sort(), Float::sort())))),
+  writableMaps(ctx.constant("writableMap",
+    ctx.array_sort(ctx.bv_sort(BID_BITS), ctx.bool_sort()))),
+  numelemMaps(ctx.constant("numelemMap",
+    ctx.array_sort(ctx.bv_sort(BID_BITS), Index::sort()))) {}
+
+NewMemory::MemBlock NewMemory::getMemBlock(const z3::expr &bid) const {
+  z3::expr array(ctx);
+  if (NUM_BLOCKS == 1) {
+    array = arrayMaps[0];
+  } else {
+    // Z3 if-then-else encoding
+    assert(NUM_BLOCKS >= 2);
+    array = z3::ite(
+      bid == ctx.bv_val(NUM_BLOCKS - 2, BID_BITS),
+      arrayMaps[NUM_BLOCKS - 2], arrayMaps[NUM_BLOCKS - 1]);
+    for (int i = (int)NUM_BLOCKS - 3; i >= 0; i --)
+      array = z3::ite(bid == ctx.bv_val(i, BID_BITS), arrayMaps[i], array);
+  }
+  z3::expr writable = z3::select(writableMaps, bid);
+  z3::expr numelem = z3::select(numelemMaps, bid);
+  return MemBlock(array, writable, numelem);
+}
+
+z3::expr NewMemory::getNumElementsOfMemBlock(const z3::expr &bid) const {
+  return getMemBlock(bid).numelem;
+}
+
+void NewMemory::updateMemBlock(const z3::expr &bid, bool writable) {
+  z3::store(writableMaps, bid, ctx.bool_val(writable));
+}
+
+z3::expr NewMemory::store(const z3::expr &f32val, const z3::expr &bid, const z3::expr &idx) {
+  const auto block = getMemBlock(bid);
+  for (int i = 0; i < NUM_BLOCKS; i ++) {
+    arrayMaps[i] = z3::ite(
+      bid == ctx.bv_val(i, BID_BITS),
+      z3::store(block.array, idx, f32val), arrayMaps[i]);
+  }
+  return z3::ult(idx, block.numelem) && block.writable;
+}
+
+std::pair<z3::expr, z3::expr> NewMemory::load(const z3::expr &bid, const z3::expr &idx) const {
+  const auto block = getMemBlock(bid);
+  return {z3::select(block.array, idx), z3::ult(idx, block.numelem)};
+}
--- src/memory.h
+++ src/memory.h
@@ -3,6 +3,7 @@
 #include "z3++.h"
 
 #include <algorithm>
+#include <vector>
 
 class Memory {
   z3::expr arrayMap; // bv(2)::sort() -> (Index::sort() -> Float::sort())
@@ -28,9 +29,9 @@ public:
 
   Memory();
 
-  void updateMemBlock(const z3::expr &bid, bool writable);
-
   z3::expr getNumElementsOfMemBlock(const z3::expr &bid) const;
+
+  void updateMemBlock(const z3::expr &bid, bool writable);
 
   // Returns: store successful?
   z3::expr store(const z3::expr &f32val, const z3::expr &bid, const z3::expr &idx);
@@ -38,21 +39,37 @@ public:
   std::pair<z3::expr, z3::expr> load(const z3::expr &bid, const z3::expr &idx) const;
 };
 
-// class NewMemory {
-//   std::vector<z3::expr> arrayMaps; // bv(2) -> (Index::sort() -> Float::sort()) 
-//   z3::expr writableMaps; // bv(2) -> Bool::sort()
-//   z3::expr numelemMaps; // bv(2) -> Index::sort
+class NewMemory {
+  unsigned int NUM_BLOCKS;
+  std::vector<z3::expr> arrayMaps; // bv(2) -> (Index::sort() -> Float::sort()) 
+  z3::expr writableMaps; // bv(2) -> Bool::sort()
+  z3::expr numelemMaps; // bv(2) -> Index::sort
 
-// public:
-//   static const unsigned BID_BITS = 1;
-//   NewMemory();
+  // A memory block containing f32 elements.
+  class MemBlock {
+  public:
+    z3::expr array;    // Index::sort() -> Float::sort()
+    z3::expr writable; // bool::sort()
+    z3::expr numelem;  // Index::sort()
 
-//   MemBlock getMemBlock(const z3::expr &bid) const;
+    MemBlock(z3::expr &array, z3::expr &writable, z3::expr &numelem):
+      array(array), writable(writable), numelem(numelem) {}
+  };
 
-//   void updateMemBlock(const z3::expr &bid, bool writable);
+private:
+  MemBlock getMemBlock(const z3::expr &bid) const;
 
-//   // Returns: store successful?
-//   z3::expr store(const z3::expr &f32val, const z3::expr &bid, const z3::expr &idx);
-//   // Returns: (loaded value, load successful?)
-//   std::pair<z3::expr, z3::expr> load(const z3::expr &bid, const z3::expr &idx) const;
-// }
+public:
+  static const unsigned BID_BITS = 2;
+
+  NewMemory(unsigned int NUM_BLOCKS);
+
+  z3::expr getNumElementsOfMemBlock(const z3::expr &bid) const;
+
+  void updateMemBlock(const z3::expr &bid, bool writable);
+
+  // Returns: store successful?
+  z3::expr store(const z3::expr &f32val, const z3::expr &bid, const z3::expr &idx);
+  // Returns: (loaded value, load successful?)
+  std::pair<z3::expr, z3::expr> load(const z3::expr &bid, const z3::expr &idx) const;
+};
