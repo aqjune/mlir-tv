9285ea8fb698588ac0184e445346c51d6388edbd
Seunghyeon Nam <kuguakrguakr@gmail.com>
2021-07-26 17:55:02 +0900

    More refactoring & added identity check step

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -9,4 +9,4 @@ pass_name: str = lit_config.params["pass"]
 config.name = 'MLIR'
 config.test_source_root = os.path.join(config.my_src_root, f"tests/{root_name}")
 config.test_exec_root = os.path.join(config.my_obj_root, "tests")
-config.test_format = lit.formats.PairTest(iree_tv, pass_name)
+config.test_format = lit.formats.SrcTgtPairTest(iree_tv, pass_name)
--- tests/lit/formats/mlirtest.py
+++ tests/lit/formats/mlirtest.py
@@ -1,7 +1,7 @@
 from enum import Enum, auto
 from lit.formats.base import TestFormat
 import lit
-from lit.Test import *
+from lit.Test import ResultCode
 
 from typing import Tuple
 from abc import ABC, abstractmethod
@@ -10,7 +10,8 @@ import os
 import re
 import signal
 
-def _executeCommand(command: list[str]):
+def _executeCommand(dir_tv: str, dir_src: str, dir_tgt: str, timeout: int = 10000) -> Tuple[str, str, int]:
+    command: list[str] = [dir_tv, f"-smt-to={timeout}", dir_src, dir_tgt]
     with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8") as proc:
         exitCode = proc.wait()
 
@@ -23,9 +24,9 @@ def _executeCommand(command: list[str]):
     return out, err, exitCode
 
 def _includes_unsupported_message(errs: str) -> bool:
-    unsupported_msg_keywords: list[str] = ["Unknown"]
+    unsupported_msg_keywords: list[str] = "Unknown"
     for keyword in unsupported_msg_keywords:
-        if keyword in errs:
+        if keyword == errs:
             return True
     return False
 
@@ -41,9 +42,19 @@ class TestBase(ABC):
     def __init__(self, keyword: TestKeyword) -> None:
         self.__keyword: TestKeyword = keyword
 
-    def check_exit_code(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
+    @abstractmethod
+    def check_exit_code(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
+        pass
+
+    def __eq__(self, other: TestKeyword) -> bool:
+        return self.__keyword == other
+
+class ExitCodeDependentTestBase(TestBase):
+    def __init__(self, keyword: TestKeyword) -> None:
+        super().__init__(keyword)
+
+    def check_exit_code(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
         if exit_code == 1:
-            # keyword-independent results
             if _includes_unsupported_message(errs):
                 return lit.Test.UNRESOLVED, ""
             else:
@@ -51,104 +62,62 @@ class TestBase(ABC):
         elif exit_code >= 65 and exit_code <= 68:
             return lit.Test.UNRESOLVED, ""
         else:
-            return self._keyword_dependent_checks(outs, errs, exit_code)
-    
+            return self._check(outs, errs, exit_code)
+
     @abstractmethod
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
+    def _check(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
         pass
 
-    def __eq__(self, other: TestKeyword) -> bool:
-        return self.__keyword == other
+class FixedResultTestBase(TestBase):
+    def __init__(self, keyword: TestKeyword) -> None:
+        super().__init__(keyword)
 
-class NoTest(TestBase):
+class NoTest(FixedResultTestBase):
     def __init__(self):
         super().__init__(TestKeyword.NOTEST)
 
-    def check_exit_code(self, outs, errs, exit_code) -> Tuple[lit.Test.Test, str]:
+    def check_exit_code(self, outs, errs, exit_code) -> Tuple[ResultCode, str]:
         return lit.Test.UNRESOLVED, ""
 
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
-        # unimplemented
-        pass
+class UnsupportedTest(FixedResultTestBase):
+    def __init__(self):
+        super().__init__(TestKeyword.UNSUPPORTED)
 
-class VerifyTest(TestBase):
+    def check_exit_code(self, outs, errs, exit_code) -> Tuple[ResultCode, str]:
+        return lit.Test.UNSUPPORTED, ""
+
+class VerifyTest(ExitCodeDependentTestBase):
     def __init__(self):
         super().__init__(TestKeyword.VERIFY)
 
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
+    def _check(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
         if exit_code == 0:
             return lit.Test.PASS, ""
         else:
             return lit.Test.FAIL, f"stdout >>\n{outs}\n\nstderr >>\n{errs}"
 
-class VerifyIncorrectTest(TestBase):
+class VerifyIncorrectTest(ExitCodeDependentTestBase):
     def __init__(self):
         super().__init__(TestKeyword.VERIFY_INCORRECT)
 
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
+    def _check(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
         if exit_code == 0:
             return lit.Test.FAIL, "This test must fail!"
         else:
             return lit.Test.PASS, ""
 
-class UnsupportedTest(TestBase):
-    def __init__(self):
-        super().__init__(TestKeyword.UNSUPPORTED)
-
-    def check_exit_code(self, outs, errs, exit_code) -> Tuple[lit.Test.Test, str]:
-        return lit.Test.UNSUPPORTED, ""
-
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
-        # unimplemented
-        pass
-
-class ExpectTest(TestBase):
+class ExpectTest(ExitCodeDependentTestBase):
     def __init__(self, msg: str):
         super().__init__(TestKeyword.EXPECT)
         self.__msg: str = msg
 
-    def _keyword_dependent_checks(self, outs: str, errs: str, exit_code: int) -> Tuple[lit.Test.Test, str]:
+    def _check(self, outs: str, errs: str, exit_code: int) -> Tuple[ResultCode, str]:
         if self.__msg in outs or self.__msg in errs:
             return lit.Test.PASS, ""
         else:
             return lit.Test.FAIL, f"Expected message >>\n{self.__msg}\n\nstdout >>\n{outs}\n\nstderr >>\n{errs}"
 
-class MLIRTest(TestFormat):
-    def __init__(self, dir_tv: str, pass_name: str) -> None:
-        self._dir_tv: str = dir_tv
-        self._pass_name: str = pass_name
-
-    @abstractmethod
-    def _test_dependent_passname_filter(self, pass_name: str) -> bool:
-        pass
-
-    @abstractmethod
-    def _test_dependent_casename_filter(self, case_name: str) -> bool:
-        pass
-
-    @abstractmethod
-    def _test_dependent_casename_modifier(self, case_name: str) -> str:
-        pass
-
-    def getTestsInDirectory(self, testSuite, path_in_suite, litConfig, localConfig):
-        source_path = testSuite.getSourcePath(path_in_suite)
-        for pass_name in filter(lambda name: (os.path.isdir(os.path.join(source_path, name)) \
-                    and not name.startswith('.') \
-                    and self._test_dependent_passname_filter(name))
-                , os.listdir(source_path)):
-            pass_path: str = os.path.join(source_path, pass_name)
-            for case_name in filter(lambda name: (os.path.isfile(os.path.join(pass_path, name)) \
-                        and not name.startswith('.') \
-                        and self._test_dependent_casename_filter(name))
-                    , os.listdir(pass_path)):
-                yield lit.Test.Test(testSuite, path_in_suite 
-                    + (os.path.join(pass_name, self._test_dependent_casename_modifier(case_name)),), localConfig)
-
-    @abstractmethod
-    def execute(self, test, litConfig) -> Tuple[lit.Test.Test, str]:
-        pass
-
-class PairTest(MLIRTest):
+class SrcTgtPairTest(TestFormat):
     __suffix_src: str = ".src.mlir"
     __suffix_tgt: str = ".tgt.mlir"
     __verify_regex = re.compile(r"^// ?VERIFY$")
@@ -157,24 +126,38 @@ class PairTest(MLIRTest):
     __expect_regex = re.compile(r"^// ?EXPECT ?: ?\"(.*)\"$")
 
     def __init__(self, dir_tv: str, pass_name: str) -> None:
-        super().__init__(dir_tv, pass_name)
+        self._dir_tv: str = dir_tv
+        self._pass_name: str = pass_name
 
-    def _test_dependent_passname_filter(self, pass_name: str) -> bool:
-        return pass_name == self._pass_name
+    def getTestsInDirectory(self, testSuite, path_in_suite, litConfig, localConfig):
+        source_path = testSuite.getSourcePath(path_in_suite)
+        for pass_name in filter(lambda name: (os.path.isdir(os.path.join(source_path, name)) \
+                    and not name.startswith('.') \
+                    and name == self._pass_name)
+                , os.listdir(source_path)):
+            pass_path: str = os.path.join(source_path, pass_name)
+            for case_name in filter(lambda name: (os.path.isfile(os.path.join(pass_path, name)) \
+                        and not name.startswith('.') \
+                        and name.endswith(self.__suffix_src))
+                    , os.listdir(pass_path)):
+                yield lit.Test.Test(testSuite, path_in_suite 
+                    + (os.path.join(pass_name, case_name.removesuffix(self.__suffix_src)),), localConfig)
 
-    def _test_dependent_casename_filter(self, case_name: str) -> bool:
-        return case_name.endswith(self.__suffix_src)
-
-    def _test_dependent_casename_modifier(self, case_name: str) -> str:
-        return case_name.removesuffix(self.__suffix_src)
-
-    def execute(self, test, litConfig) -> Tuple[lit.Test.Test, str]:
+    def execute(self, test, litConfig) -> Tuple[ResultCode, str]:
         test = test.getSourcePath()
         tc_src = test + self.__suffix_src
         tc_tgt = test + self.__suffix_tgt
         if not (os.path.isfile(tc_src) and os.path.isfile(tc_tgt)):
             # src or tgt mlir file is missing
-            return lit.Test.SKIPPED
+            return lit.Test.SKIPPED, ""
+
+        src_identity: Tuple[ResultCode, str] = VerifyTest().check_exit_code(*_executeCommand(self._dir_tv, tc_src, tc_src))
+        if src_identity[0] != lit.Test.PASS:
+            return src_identity
+
+        tgt_identity: Tuple[ResultCode, str] = VerifyTest().check_exit_code(*_executeCommand(self._dir_tv, tc_tgt, tc_tgt))
+        if tgt_identity[0] != lit.Test.PASS:
+            return tgt_identity
 
         test: TestBase = NoTest()
         with open(tc_src, 'r') as src_file:
@@ -193,5 +176,4 @@ class PairTest(MLIRTest):
                     test = ExpectTest(msg)
                     break
 
-        cmd = [self._dir_tv, "-smt-to=10000", tc_src, tc_tgt]
-        return test.check_exit_code(*_executeCommand(cmd))
+        return test.check_exit_code(*_executeCommand(self._dir_tv, tc_src, tc_tgt))
