5205b1e499f7e3bc9433b55437f74ffad284b3ba
makesource <makesource@gmail.com>
2021-08-09 20:11:23 +0900

    Apply code review

--- src/vcgen.cpp
+++ src/vcgen.cpp
@@ -447,17 +447,16 @@ optional<string> encodeOp(State &st, mlir::memref::BufferCastOp op) {
     auto success = memref.storeArray(tensor.asArray(), Index::zero(), tensor.get1DSize());
     st.wellDefined(success);
     st.regs.add(op.memref(), move(memref));
-    return {};
+  } else {
+    vector<expr> idxs = createIndexVars(memrefTy.getRank());
+    auto tVal = tensor.get(idxs);
+    auto [mVal, success] = memref.load(idxs);
+    memref.setWritable(false);
+
+    st.wellDefined(z3::forall(toExprVector(idxs), z3::implies(success, mVal == tVal)));
+    st.hasQuantifier = true;
+    st.regs.add(op.memref(), move(memref));
   }
-
-  vector<expr> idxs = createIndexVars(memrefTy.getRank());
-  auto tVal = tensor.get(idxs);
-  auto [mVal, success] = memref.load(idxs);
-  memref.setWritable(false);
-
-  st.wellDefined(z3::forall(toExprVector(idxs), z3::implies(success, mVal == tVal)));
-  st.hasQuantifier = true;
-  st.regs.add(op.memref(), move(memref));
   return {};
 }
 
