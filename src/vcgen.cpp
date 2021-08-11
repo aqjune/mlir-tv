233b511f1f6fa6f3ce48d6be7ccd22f2dd50bf88
KKimj <kkimj@hanyang.ac.kr>
2021-08-11 14:53:37 +0900

    Update : support Sparse Tensor Constant

--- src/vcgen.cpp
+++ src/vcgen.cpp
@@ -670,6 +670,28 @@ optional<string> encodeOp(State &st, mlir::ConstantOp op) {
 
     st.regs.add(op, Integer(i.getSExtValue(), bw));
     return {};
+  } else if(mlir::SparseElementsAttr sparseAttr = attr.dyn_cast<mlir::SparseElementsAttr>()) {
+    std::vector<z3::expr> sparse_values;
+    mlir::Type eltType = sparseAttr.getType().getElementType();
+    if(eltType.isa<mlir::FloatType>()) {
+      auto values = sparseAttr.getValues<mlir::FloatAttr>();
+      for(mlir::FloatAttr value : values) {
+        z3::expr float_value = Float(value.getValueAsDouble());
+        sparse_values.push_back(float_value);
+      }
+    } else if(eltType.isa<mlir::IntegerType>()) {
+      auto values = sparseAttr.getValues<mlir::IntegerAttr>();
+      for(mlir::IntegerAttr value : values) {
+        llvm::APInt i = value.getValue();
+        unsigned bw = i.getBitWidth();
+        z3::expr int_value = Integer(i.getSExtValue(), bw);
+        sparse_values.push_back(int_value);
+      }
+    }
+    else 
+      return "unsupported type";
+    st.regs.add(op, Tensor(sparse_values));
+    return {};
   }
   return "unsupported constant";
 }
