2bcd07ec0dff65be1cb8ac15c92fc7e31c6aa09f
Juneyoung Lee <aqjune@gmail.com>
2021-12-27 17:33:34 +0900

    Optimize udiv/urem by pow(2,n)

--- src/smt.cpp
+++ src/smt.cpp
@@ -446,8 +446,14 @@ Expr Expr::urem(const Expr &rhs) const {
   CHECK_LOCK2(rhs);
 
   uint64_t rhsval;
-  if (rhs.isUInt(rhsval) && rhsval == 1)
-    return Expr::mkBV(0, rhs);
+  if (rhs.isUInt(rhsval)) {
+    if (rhsval == 1)
+      return Expr::mkBV(0, rhs);
+    else if (rhsval > 0 && (rhsval & (rhsval - 1)) == 0) {
+      uint64_t l = log2_ceil(rhsval);
+      return Expr::mkBV(0, bitwidth() - l).concat(extract(l - 1, 0));
+    }
+  }
 
   uint64_t a, b;
   // If divisor is zero, follow the solver's behavior
@@ -466,8 +472,14 @@ Expr Expr::udiv(const Expr& rhs) const {
   CHECK_LOCK2(rhs);
 
   uint64_t rhsval;
-  if (rhs.isUInt(rhsval) && rhsval == 1)
-    return *this;
+  if (rhs.isUInt(rhsval)) {
+    if (rhsval == 1)
+      return *this;
+    else if (rhsval > 0 && (rhsval & (rhsval - 1)) == 0) {
+      uint64_t l = log2_ceil(rhsval);
+      return Expr::mkBV(0, l).concat(extract(bitwidth() - 1, l));
+    }
+  }
 
   uint64_t a, b;
   // If divisor is zero, follow the solver's behavior
