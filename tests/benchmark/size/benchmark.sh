cf10d34e9d08abfe04299f1e6e5f76a1b3e15e41
makesource <makesource@gmail.com>
2022-01-11 11:41:25 +0900

    fix

--- tests/benchmark/size/benchmark.sh
+++ tests/benchmark/size/benchmark.sh
@@ -1,34 +1,34 @@
 ./mlir-tv ../tests/benchmark/size/1.dot_reverse_variable_10.src.mlir ../tests/benchmark/size/1.dot_reverse_variable_10.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/1.dot_reverse_variable_10.src.mlir ../tests/benchmark/size/1.dot_reverse_variable_10.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/1.dot_reverse_variable_10.src.mlir ../tests/benchmark/size/1.dot_reverse_variable_10.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/1.dot_reverse_variable_10.src.mlir ../tests/benchmark/size/1.dot_reverse_variable_10.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/1.dot_reverse_variable_10.src.mlir ../tests/benchmark/size/1.dot_reverse_variable_10.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/2.dot_reverse_variable_20.src.mlir ../tests/benchmark/size/2.dot_reverse_variable_20.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/2.dot_reverse_variable_20.src.mlir ../tests/benchmark/size/2.dot_reverse_variable_20.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/2.dot_reverse_variable_20.src.mlir ../tests/benchmark/size/2.dot_reverse_variable_20.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/2.dot_reverse_variable_20.src.mlir ../tests/benchmark/size/2.dot_reverse_variable_20.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/2.dot_reverse_variable_20.src.mlir ../tests/benchmark/size/2.dot_reverse_variable_20.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/3.dot_reverse_variable_50.src.mlir ../tests/benchmark/size/3.dot_reverse_variable_50.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/3.dot_reverse_variable_50.src.mlir ../tests/benchmark/size/3.dot_reverse_variable_50.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/3.dot_reverse_variable_50.src.mlir ../tests/benchmark/size/3.dot_reverse_variable_50.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/3.dot_reverse_variable_50.src.mlir ../tests/benchmark/size/3.dot_reverse_variable_50.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/3.dot_reverse_variable_50.src.mlir ../tests/benchmark/size/3.dot_reverse_variable_50.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/4.dot_reverse_variable_100.src.mlir ../tests/benchmark/size/4.dot_reverse_variable_100.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/4.dot_reverse_variable_100.src.mlir ../tests/benchmark/size/4.dot_reverse_variable_100.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/4.dot_reverse_variable_100.src.mlir ../tests/benchmark/size/4.dot_reverse_variable_100.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/4.dot_reverse_variable_100.src.mlir ../tests/benchmark/size/4.dot_reverse_variable_100.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/4.dot_reverse_variable_100.src.mlir ../tests/benchmark/size/4.dot_reverse_variable_100.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/5.dot_reverse_variable_200.src.mlir ../tests/benchmark/size/5.dot_reverse_variable_200.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/5.dot_reverse_variable_200.src.mlir ../tests/benchmark/size/5.dot_reverse_variable_200.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/5.dot_reverse_variable_200.src.mlir ../tests/benchmark/size/5.dot_reverse_variable_200.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/5.dot_reverse_variable_200.src.mlir ../tests/benchmark/size/5.dot_reverse_variable_200.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/5.dot_reverse_variable_200.src.mlir ../tests/benchmark/size/5.dot_reverse_variable_200.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/6.dot_reverse_variable_1000.src.mlir ../tests/benchmark/size/6.dot_reverse_variable_1000.tgt.mlir --associative
-./mlir-tv ../tests/benchmark/size/6.dot_reverse_variable_1000.src.mlir ../tests/benchmark/size/6.dot_reverse_variable_1000.tgt.mlir --associative --multiset
+./mlir-tv ../tests/benchmark/size/6.dot_reverse_variable_1000.src.mlir ../tests/benchmark/size/6.dot_reverse_variable_1000.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/6.dot_reverse_variable_1000.src.mlir ../tests/benchmark/size/6.dot_reverse_variable_1000.tgt.mlir --associative --solver=CVC5
 ./mlir-tv ../tests/benchmark/size/6.dot_reverse_variable_1000.src.mlir ../tests/benchmark/size/6.dot_reverse_variable_1000.tgt.mlir --associative --multiset --solver=CVC5
 
 ./mlir-tv ../tests/benchmark/size/7.dot_reverse_variable_10000.src.mlir ../tests/benchmark/size/7.dot_reverse_variable_10000.tgt.mlir --associative
 ./mlir-tv ../tests/benchmark/size/7.dot_reverse_variable_10000.src.mlir ../tests/benchmark/size/7.dot_reverse_variable_10000.tgt.mlir --associative --multiset --smt-use-all-logic
 ./mlir-tv ../tests/benchmark/size/7.dot_reverse_variable_10000.src.mlir ../tests/benchmark/size/7.dot_reverse_variable_10000.tgt.mlir --associative --solver=CVC5
-./mlir-tv ../tests/benchmark/size/7.dot_reverse_variable_10000.src.mlir ../tests/benchmark/size/7.dot_reverse_variable_10000.tgt.mlir --associative --solver=CVC5 --multiset --solver=CVC5
+./mlir-tv ../tests/benchmark/size/7.dot_reverse_variable_10000.src.mlir ../tests/benchmark/size/7.dot_reverse_variable_10000.tgt.mlir --associative --solver=CVC5 --multiset
