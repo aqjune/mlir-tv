d75ef7d9aaba7aa1d2ca500855c25246e999b65e
strikef <seunghyeon.nam@sf.snu.ac.kr>
2022-01-16 12:13:20 +0900

    Fix typo in comment

--- src/abstractops.cpp
+++ src/abstractops.cpp
@@ -1295,7 +1295,7 @@ Expr AbsFpEncoding::getFpConstantPrecondition() {
   Expr precond = Expr::mkBool(true);
   
   auto prev_fp = llvm::APFloat::getInf(semantics, true);
-  // both largest() and infinity() are hardcoded hardcoded value,
+  // both largest() and infinity() are hardcoded value,
   // so we don't have to explicitly encode the relationship between them
   auto prev_absrepr = largest(true);
   bool firstItr = true;
